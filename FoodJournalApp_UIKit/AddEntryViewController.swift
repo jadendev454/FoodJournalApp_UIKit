bf4f6b2c42664677417ec93a0fbc6baed174dd5e
jadendev454 <jadendev454@gmail.com>
2022-11-27 23:04:19 +0200

    Additional Functionality (Add Entry) with constraints setup

---
+++ FoodJournalApp_UIKit/AddEntryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddEntryViewController.swift
+//  FoodJournalApp_UIKit
+//
+//  Created by IACD 05 on 2022/11/27.
+//
+
+import UIKit
+import Foundation
+
+class AddEntryViewController: UIViewController {
+
+    
+    @IBOutlet weak var addItemBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
